57119c1cd8b8267277936df8c1d2bbccd2265180
Chamaththa123 <shamodchamaththa@gmail.com>
2025-03-27 20:50:38 +0530

    integrate with docker

--- gateway/server.js
+++ gateway/server.js
@@ -5,8 +5,8 @@ const app = express();
 
 // Define your services and their corresponding ports
 const services = [
-  { route: '/users', target: 'http://localhost:3001', serviceName: 'User Service' },
-  { route: '/orders', target: 'http://localhost:3002', serviceName: 'Order Service' }
+  { route: '/users', target: 'http://user-service:3001', serviceName: 'User Service' },  // Added serviceName
+  { route: '/orders', target: 'http://order-service:3002', serviceName: 'Order Service' } // Added serviceName
 ];
 
 // Loop through services to create routes and proxy middleware dynamically
