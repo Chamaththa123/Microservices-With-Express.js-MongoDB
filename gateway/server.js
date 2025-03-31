60709e96a6d91e7a38c76329579894143783c364
Chamaththa123 <shamodchamaththa@gmail.com>
2025-03-31 23:23:12 +0530

    Update server.js

--- gateway/server.js
+++ gateway/server.js
@@ -5,8 +5,8 @@ const app = express();
 
 // Define your services and their corresponding ports
 const services = [
-  { route: '/users', target: 'http://user-service:3001', serviceName: 'User Service' },  // Added serviceName
-  { route: '/orders', target: 'http://order-service:3002', serviceName: 'Order Service' } // Added serviceName
+  { route: '/users', target: 'http://user-service.default.svc.cluster.local:3001', serviceName: 'User Service' },  // Added serviceName
+  { route: '/orders', target: 'http://order-service.default.svc.cluster.local:3002', serviceName: 'Order Service' } // Added serviceName
 ];
 
 // Loop through services to create routes and proxy middleware dynamically
